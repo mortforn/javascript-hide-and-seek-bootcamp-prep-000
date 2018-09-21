ee83ccd07df291106d98ff36128287e656e1f62c
Matthew Fine <mfine@wesleyan.edu>
2018-09-21 10:19:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+  const selected = document.querySelector(selector);
+  return selected;
+}
 
+function nestedTarget() {
+  const nestedTarget = document.querySelector('div#nested div.target');
+  return nestedTarget;
+}
+
+function deepestChild() {
+ let deepChild = document.querySelectorAll('div#grand-note');
+    for (let i = 0; i < deepChild.length; i++) {
+      deepChild = deepChild[deepchild.length - 1];
+      return deepChild;
+    }
+}
+
+function increaseRankBy(n) {
+  const newList = document.getElementById('app').querySelectorAll('ul.ranked-list');
+  for (let i = 0; i < newList.length; i++) {
+    newList[i].innerHTML = (i + 1).toString();
+  }
+}
